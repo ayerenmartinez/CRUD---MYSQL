f65824f5e0cce66db74d876458439be7a185c58a
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2022-09-10 13:04:00 -0500

    Creación del Proyecto Presentación y Creación del Formulario frmArticulos

--- CRUD - MYSQL/Program.cs
+++ CRUD - MYSQL/Program.cs
@@ -16,7 +16,7 @@ namespace CRUD___MYSQL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmArticulos());
         }
     }
 }
