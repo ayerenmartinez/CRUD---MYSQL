602cdeb90952614a00c2a657ad13cdc907a2c2d7
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2022-09-10 13:57:30 -0500

    Crear clase clsConexion

---
+++ CRUD - MYSQL/clsConexion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace CRUD___MYSQL
+{
+    public class clsConexion
+    {
+        private String Base;
+        private String Servidor;
+        private String Puerto;
+        private String Usuario;
+        private String Clave;
+        private static clsConexion cnx = null;
+
+        private clsConexion()
+        {
+            this.Base = "bdAlmacen";
+            this.Servidor = "localhost";
+            this.Puerto = "3306";
+            this.Usuario = "root";
+            this.Clave = "";
+        }
+        public MySqlConnection CrearConexion()
+        {
+            MySqlConnection Cadena = new MySqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "datasource=" + this.Servidor +
+                                        "; port=" + this.Puerto +
+                                        "; username=" + this.Usuario +
+                                        "; password=" + this.Clave +
+                                        "; Database=" + this.Base;
+            }
+            catch (Exception ex)
+            {
+                Cadena = null; //Si hubiera algun problema con la conexión la hacemos null
+                throw ex;
+            }
+            return Cadena;
+        }
+        public static clsConexion getInstancia()
+        {
+            if (cnx ==null)
+            {
+                cnx = new clsConexion();
+            }
+            return cnx;
+        }
+    }
+}
